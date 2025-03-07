7d40948cbfe9236f46faf2b6eb2fc6e4e8aec710
vidya <vidya.palliyalil@gmail.com>
2025-03-08 01:03:10 +0530

    add getAllCustomers endpoint to retrieve non-deleted customers

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -43,5 +43,25 @@ module.exports = {
                 data: error
             })
         }
+    },
+    getAllCustomers: async(req, res) => {
+        try {
+            const customers = await customerModel.find({isDeleted: false});
+            console.log(customers);
+
+            return res.status(200).json({
+                success: true,
+                statuscode: 200,
+                message: "All customers",
+                data: customers
+            })
+        } catch (error) {
+            return res.status(500).json({
+                success: false,
+                statuscode: 500,
+                message: "Internal Server Error",
+                data: error
+            })
+        }
     }
 }
