db47e43f73bda317b7771e3a7416f080664c6045
vidya <vidya.palliyalil@gmail.com>
2025-03-08 23:37:35 +0530

    refactor database connection to use async/await and improve error handling

--- dbConfig/index.js
+++ dbConfig/index.js
@@ -2,10 +2,25 @@ const mongoose = require('mongoose');
 
 const mongoDBUri = process.env.MONGODB_STRING;
 
-mongoose
-    .connect(mongoDBUri)
-    .then((response) => {
-        console.log("EntryApp Database connected");
-    }).catch((err) => {
-        console.log("connction error: ", err);
-    });
+// mongoose
+//     .connect(mongoDBUri)
+//     .then((response) => {
+//         console.log("EntryApp Database connected");
+//     }).catch((err) => {
+//         console.log("Database connction error: ", err);
+//     });
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(mongoDBUri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+    } catch (error) {
+        console.log("error:", error);
+        process.exit(1);
+    }
+};
+module.exports = connectDB
+
+
